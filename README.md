d947494fb732783f8d8fca67718c04551585c36a
Nalin Gabriel <nalin.gaby@gmail.com>
2017-10-29 14:44:37 +0530

    commit for motor controller

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # projectcoconut
 Arduino code to drive 2 servos
+
+
+MH motor shield
+A5 used for pot input
+D9, D10 for servo controller signal outputs
---
+++ nalin_servo_code.ino
@@ -0,0 +1,35 @@
+// Nalin wrote code to control servo motor - 
+// Towerpro SG90
+// Before running the code, do below:
+// - Connect the servo yellow to pin 9, r/b to +5/gnd
+
+#include <Servo.h> 
+int potPin = A5;
+Servo servo1;
+Servo servo2;
+
+void setup()
+{
+servo1.attach(10);
+servo2.attach(9);
+       
+    // Start up serial connection
+    Serial.begin(9600); // baud rate
+    Serial.flush();
+}
+
+void loop()
+{
+  // analog read - changes from 0 to 1023
+   int potValue =  analogRead(potPin); 
+   
+   Serial.println(potValue); 
+
+      int pottoservo = potValue/6;
+      servo1.write (pottoservo);
+   delay(100);
+      servo2.write (180 - pottoservo);
+      //digitalWrite (9, HIGH);
+      delay (1000);   
+} 
+   
